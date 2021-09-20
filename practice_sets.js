74f66ea33113f05c865ec91df9575e7f8d880794
Yamato Tadokoro <miynevfor311jap@gmail.com>
2021-09-20 16:21:11 -0700

    finished the basic find all operations

--- practice_sets.js
+++ practice_sets.js
@@ -14,43 +14,59 @@ db.students.insertMany([
 ///////////////////// Practice Sets ////////////////////////
 ////////////////////////////////////////////////////////////
 
-
+coll = db.students;
 // Find all students
+coll.find()
 
 // Find the first 3 students
+coll.find().limit(3)
 
 // Find all students and sort by name in ascending order
+coll.find({}).sort({"name": 1})
 
-// Find all students and sort by name in ascending order
+// Find all students and sort by name in ascending order and gpa in descending order
+coll.find({}).sort({"gpa": -1, "name": 1})
 
 // Find all biology majors
+coll.find({"major": "Biology"})
 
 // Find all student's with a phone number 333-3333
+coll.find({"contact.phone": "333-3333"})
 
 // Find all biology majors named Jack
+coll.find({ $and: [{"major": "Biology"}, {"name": "Jack"}]})
+coll.find({"major": "Biology", "name": "Jack"})
 
 // Final all students who are chemistry majors or named Jack
+coll.find({ $or: [{"major": "Chemistry"}, {"name": "Jack"}]})
 
 // Final all students with a gpa above 3.5
+coll.find( {gpa: {$gt: 3.5} } )
 
 // Find all students with a gpa less than or equal to 3.2
                 // $eq, $ne, $lt, $lte, $gt, $gte
+coll.find({gpa: { $lte: 3.2}})
 
 // Find all students with names in the array
   // $in, $nin
+coll.find( {name: {$in: ["Kate", "Claire"]} } )
 
 // Find all students who have awards
          // false
+coll.find({awards: { $exists: true}})
 
 // Find all db entries where the name is a string
 // Type list - https://docs.mongodb.com/manual/reference/bson-types/
-
+coll.find({name: {$type: 2} })
+// change the number after the $type to see the different types
 
 // Find all students who's first grade is a 90
+coll.find({"grades.0": 90})
 
 // Find all students who have a grade greater than 80
+coll.find({grades : { $elemMatch: { $gte: 80}}})
 
 // Find all students who have 4 grades recorded
-
+coll.find({grades: {$size: 4}})
 
 
