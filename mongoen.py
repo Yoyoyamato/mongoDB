ad309378a4bee7bad3355664056d60ce314ad9c5
Yamato Tadokoro <miynevfor311jap@gmail.com>
2021-09-21 09:45:38 -0700

    uploading file using mongoengine to interact with mongodb using python

--- mongoen.py
+++ mongoen.py
@@ -110,7 +110,119 @@ except NotUniqueError:
 
 users = User.objects()
 
-print(users)
+for user in users:
+    print(user.username, user.email, user.bio)
+
+# Filtering
+
+admin_user = User.objects(admin = True)
+
+for a in admin_user:
+    print(a.username)
+
+admin_user = User.objects(admin = True, registered = True)
+
+for a in admin_user:
+    print(a.username)
+
+# getting an uique result
+john_doe = User.objects(username = "JohnDoe").get()
+print(john_doe.username, john_doe.email)
+
+
+john_doe = User.objects(username = "JohnDoe").get()
+
+posts = BlogPost.objects(author = john_doe)
+for post in posts:
+    print(post.author.username)
+
+# getting the json file for username john doe
+print(john_doe.json())
+
+
+######################################
+# Query operators
+######################################
+
+# Less than & greater than
+
+young_users = User.obejects(age__lt = 50)
+
+for user in young_users:
+    print(user.username, user.age)
+
+
+older_users = User.obejects(age__gte = 30)
+
+for user in older_users:
+    print(user.username, user.age)
+
+
+# Query that is in a list
+
+posts_tagged_python = BlogPost.objects(tags = "Python")
+
+for post in posts_tagged_python:
+    print(post.content)
+
+posts_tagged_python = BlogPost.objects(tags__in = ["Python", "MongoDB"])
+
+for post in posts_tagged_python:
+    print(post.content)
+
+# String queries
+python_posts = BlogPost.objects(content__icontains = "Python")
+
+for post in python_posts:
+    print(post.content)
+
+python_posts = BlogPost.objects(title__icontains = "fourth")
+
+for post in python_posts:
+    print(post.title)
+
+######################################
+# Limiting and skipping results
+######################################
+# Gte the first
+
+first = BlogPost.obhects().first()
+
+print(first.title)
+
+# Get the first 2 documents
+
+first_2 = BlogPost.objects()[:2]
+
+for post in first_2:
+    print(post.title)
+
+all_but = BlogPost.objects()[2:]
+for post in all_but:
+    print(post.title)
+
+sliced = BlogPost.objects()[2:4]
+for post in sliced:
+    print(post.title)
+
+
+######################################
+# Counting
+######################################
+
+user_count = User.objects().count()
+
+print(user_count)
+
+######################################
+# Aggregation
+######################################
+
+average = BlogPost.objects.average("rating")
+print(average)
+
+total_rating = BlogPost.objects.sum("rating")
+print(total_rating)
 
 
 
